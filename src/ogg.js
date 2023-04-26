052cad4cf5ea27a2184111fb9dcaf67938197a3b
Makushenko Oleksii <makushenkooleksii@gmail.com>
2023-04-27 01:17:48 +0300

    convert voice message

---
+++ src/ogg.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import {createWriteStream} from 'fs'
+import {dirname, resolve} from 'path';
+import {fileURLToPath} from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+class OggConvertor {
+  constructor() {
+  }
+
+  toMp3() {
+  }
+
+  async create(url, filename) {
+    try {
+      const oggPath = resolve(__dirname, '../voices', `${filename}.ogg`)
+      const response = await axios({
+        method: 'get',
+        url,
+        responseType: 'stream'
+      })
+      const stream = createWriteStream(oggPath);
+      response.data.pipe(stream);
+      stream.on('finish', () => {})
+    } catch (error) {
+      console.log('Error while creating ogg', error.message)
+    }
+  }
+}
+
+export const ogg = new OggConvertor();
